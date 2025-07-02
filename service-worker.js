a1befa483e249504de55a49e9db7f15b724d3da4
Gustavo Ianque <161334687+Gustavo-Ianque-Correa@users.noreply.github.com>
2025-07-02 14:49:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,15 @@
 const CACHE_NAME = 'bazaarplus-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/loja.html',
-  '/produto.html',
-  '/auth.html',
-  '/style.css',
-  '/app.js',
-  '/loja.js',
-  '/produto.js',
-  '/auth.js',
-  '/BazaarPlus_logo_website.png'
+  'index.html',
+  'loja.html',
+  'produto.html',
+  'auth.html',
+  'style.css',
+  'app.js',
+  'loja.js',
+  'produto.js',
+  'auth.js',
+  'BazaarPlus_logo_website.png'
 ];
 self.addEventListener('install', function(event) {
   event.waitUntil(
